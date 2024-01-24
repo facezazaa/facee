1078b23f48a9d4343b26ce1f003865cec9015c87
facezazaa <kophakhin155@gmail.com>
2024-01-24 10:22:43 +0700

    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -6,6 +6,12 @@ st.header("การนำเสนอข้อมูลกราฟด้วย
 
 col1,col2,col3 = st.columns(3)
 with col1:
+    st.header("Versicolor")
+    st.image("https://en.m.wikipedia.org/wiki/File:Symphyotrichum_%C3%97_versicolor.jpg")
 with col2:
+    st.header("Verginica")
+    st.image("https://commons.wikimedia.org/wiki/File:Fire_Pink_Silene_virginica_Flower_2391px.jpg")
 with col3:
+    st.header("Setosa")
+     st.image("https://commons.wikimedia.org/wiki/File:Wild_iris_flower_iris_setosa.jpg")
 
