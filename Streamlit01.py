595a787f4f164285028b14161e5b9ec569662afd
facezazaa <kophakhin155@gmail.com>
2024-01-24 10:50:01 +0700

    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -4,7 +4,7 @@ st.title("การทดสอบสร้างเว็บด้วยPython"
 st.image("data.jpeg")
 st.header("การนำเสนอข้อมูลกราฟด้วย Python") 
 
-col1,col2,col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
 with col1:
  st.header("Versicolor")
